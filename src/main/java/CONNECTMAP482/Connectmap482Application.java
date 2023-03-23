b96dfab2a684e8b0dcdf3e7cee161fdff462bf66
KKARAMCH <kushal.karamchandani@otto.de>
2023-03-23 14:30:37 +0100

    Hello World prog mit Gradle

---
+++ src/main/java/CONNECTMAP482/Connectmap482Application.java
@@ -0,0 +1,19 @@
+package CONNECTMAP482;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Connectmap482Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connectmap482Application.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/CONNECTMAP482/Connectmap482ApplicationTests.java
@@ -0,0 +1,13 @@
+package CONNECTMAP482;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Connectmap482ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
